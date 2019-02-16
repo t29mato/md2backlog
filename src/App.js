74b9e1d83a4cd34ec8c602b615d9118a58ba5de7
mato <tomoya.matou@gmail.com>
2019-02-17 02:03:30 +0900

    create convert page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      markdown: ''
+    }
+    this.onChangeMarkdown = this.onChangeMarkdown.bind(this);
+  }
+  onChangeMarkdown(e) {
+    this.setState({ markdown: e.target.value })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            Markdown To Backlog
+          </Grid>
+          <Grid item xs={6}>
+              <TextField
+                className="markdown"
+                label="Markdown"
+                multiline
+                rows="4"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                onChange={this.onChangeMarkdown}
+              />
+          </Grid>
+          <Grid item xs={6}>
+              <TextField
+                className="backlog"
+                label="Backlog"
+                multiline
+                rows="4"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                value={this.state.markdown}
+              />
+          </Grid>
+        </Grid>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme from 'enzyme';
+import { shallow, mount, render} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+import TextField from '@material-ui/core/TextField';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('textareaに文字を入力できること', () => {
+  const wrapper = shallow(<App />);
+  const input = wrapper.find('.markdown').simulate(
+    'change',
+    { target: { markdown: 'changed' } }
+  );
+
+  // expect(wrapper.state.markdown).toEqual('hello');
+  // テスト方法なぞ
+});
+
